04b159000c80d50aff78a46c79504b01518ef1df
Adbhut Satsangi <125047511+Adbhut07@users.noreply.github.com>
2024-11-18 20:39:51 +0530

    client initialisation

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+     <div className="bg-slate-500">
+      Hello
+      <div>
+        <Button>click me</Button>
+      </div>
+    </div>
+    
+  )
+}
+
+export default App
