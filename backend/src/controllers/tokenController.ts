4e66c8fb3f4114599bd220d273e627f3b6bfc789
Adbhut Satsangi <125047511+Adbhut07@users.noreply.github.com>
2024-11-27 14:28:14 +0530

    env resolve

--- backend/src/controllers/tokenController.ts
+++ backend/src/controllers/tokenController.ts
@@ -1,32 +1,32 @@
-import { RtcTokenBuilder, RtcRole } from 'agora-access-token';
-import { Request, Response } from 'express';
+// import { RtcTokenBuilder, RtcRole } from 'agora-access-token';
+// import { Request, Response } from 'express';
 
-const APP_ID = '8a17f1b47b6043a88cf582896718b905';
-const APP_CERTIFICATE = 'f013f7cdbcb8458bbd915cc9cd01ae37';
+// const APP_ID = '';
+// const APP_CERTIFICATE = '';
 
-export const generateToken =async (req: Request, res: Response):Promise<any> => {
-    const channelName = req.query.channelName as string;
-    const uid = parseInt(req.query.uid as string, 10);
-    const { role } = req.query;
+// export const generateToken =async (req: Request, res: Response):Promise<any> => {
+//     const channelName = req.query.channelName as string;
+//     const uid = parseInt(req.query.uid as string, 10);
+//     const { role } = req.query;
 
-    if (!channelName || !uid) {
-        return res.status(400).json({ error: 'Channel name and UID are required' });
-    }
+//     if (!channelName || !uid) {
+//         return res.status(400).json({ error: 'Channel name and UID are required' });
+//     }
 
-    const expirationTimeInSeconds = 3600; // 1 hour
-    const currentTimestamp = Math.floor(Date.now() / 1000);
-    const privilegeExpireTime = currentTimestamp + expirationTimeInSeconds;
+//     const expirationTimeInSeconds = 3600; // 1 hour
+//     const currentTimestamp = Math.floor(Date.now() / 1000);
+//     const privilegeExpireTime = currentTimestamp + expirationTimeInSeconds;
 
-    const rtcRole = role === 'publisher' ? RtcRole.PUBLISHER : RtcRole.SUBSCRIBER;
+//     const rtcRole = role === 'publisher' ? RtcRole.PUBLISHER : RtcRole.SUBSCRIBER;
 
-    const token = RtcTokenBuilder.buildTokenWithUid(
-        APP_ID,
-        APP_CERTIFICATE,
-        channelName,
-        uid,
-        rtcRole,
-        privilegeExpireTime
-    );
+//     const token = RtcTokenBuilder.buildTokenWithUid(
+//         APP_ID,
+//         APP_CERTIFICATE,
+//         channelName,
+//         uid,
+//         rtcRole,
+//         privilegeExpireTime
+//     );
 
-    return res.json({ token });
-};
+//     return res.json({ token });
+// };
