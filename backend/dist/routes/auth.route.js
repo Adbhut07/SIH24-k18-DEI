3936f7f32afb6ec9834f6afa9f7f0898fb6db519
Adbhut Satsangi <125047511+Adbhut07@users.noreply.github.com>
2024-11-30 17:12:40 +0530

    updated package-lock.json

--- backend/dist/routes/auth.route.js
+++ backend/dist/routes/auth.route.js
@@ -5,10 +5,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const auth_controller_1 = require("../controllers/auth/auth.controller");
-const tokenController_1 = require("../controllers/tokenController");
+;
 const router = express_1.default.Router();
 // Public routes
 router.post("/signup", auth_controller_1.signup);
 router.post("/signin", auth_controller_1.signin);
-router.get('/generate-token', tokenController_1.generateToken);
 exports.default = router;
